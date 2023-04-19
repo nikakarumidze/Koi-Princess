3a622feaa5af436eea7be6cca97395cb9f63867c
nikakarumidze <nikakarumidze@gmail.com>
2023-04-19 21:28:22 +0400

    fixed texture

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React, { ComponentProps, useState, useCallback } from 'react';
 import * as PIXI from 'pixi.js';
 import { Text, useTick, Graphics, Sprite, Stage, Container, withFilters } from '@pixi/react';
-import { slotTextures, style, backout, lineCount, lineLength, defaultSlotArr } from './consts';
+import { slotTextures, style, randomTexture, defaultSlotArr, height, width } from './consts';
 
 const REEL_WIDTH = 160;
 const SYMBOL_SIZE = 150;
-type Draw = ComponentProps<typeof Graphics>['draw'];
 
 const App = (): JSX.Element => {
-  const [app, setApp] = useState<any>();
+  const [app, setApp] = useState<PIXI.Application>();
+  const [running, setRunning] = useState(false);
   (globalThis as any).__PIXI_APP__ = app;
 
   const BlurContainer = withFilters(Container, {
@@ -18,68 +18,51 @@ const App = (): JSX.Element => {
   const slotSymbols = defaultSlotArr.map((subArr, i) => (
     <BlurContainer key={i} x={i * REEL_WIDTH} blur={{ blur: 0 }}>
       {subArr.map((l, symbolIndex) => {
-        const symbol = (
+        const texture = randomTexture();
+        return (
           <Sprite
             key={symbolIndex}
-            image='https://picsum.photos/200'
-            // image={slotTextures[Math.floor(Math.random() * slotTextures.length)]}
-            // position={[Math.round((SYMBOL_SIZE - symbol.width) / 2), symbolIndex * SYMBOL_SIZE]}
-            // scale={[Math.min(SYMBOL_SIZE / symbol.width, SYMBOL_SIZE / symbol.height)]}
+            texture={texture}
+            position={[Math.round((SYMBOL_SIZE - texture.width) / 2), symbolIndex * SYMBOL_SIZE]}
+            scale={[Math.min(SYMBOL_SIZE / texture.width, SYMBOL_SIZE / texture.width)]}
           />
         );
-        // symbol.props.x = 200;
-
-        return symbol;
       })}
     </BlurContainer>
   ));
 
-  const drawTop = useCallback(
-    (top: PIXI.Graphics) => {
-      top.beginFill(0, 1);
-      top.drawRect(0, 0, app.screen.width, (app?.screen.height - SYMBOL_SIZE * 3) / 2);
-      const text = new PIXI.Text('Spin!', style);
-      text.position = { x: 10, y: 10 };
-      text.anchor = 0.5 as any;
-      top.addChild(text);
-    },
-    [app]
-  );
-  const drawBottom = useCallback(
-    (bottom: PIXI.Graphics) => {
-      bottom.beginFill(0, 1);
-      bottom.drawRect(
-        0,
-        SYMBOL_SIZE * 3 + (app?.screen.height - SYMBOL_SIZE * 3) / 2,
-        app?.screen.width,
-        (app?.screen.height - SYMBOL_SIZE * 3) / 2
-      );
-      // const text = new PIXI.Text('Spin!', style);
-      // text.position = { x: 10, y: 10 };
-      // bottom.addChild(text);
-    },
-    [app]
-  );
-
+  const drawTop = useCallback((top: PIXI.Graphics) => {
+    top.beginFill(0, 1);
+    top.drawRect(0, 0, width, (height - SYMBOL_SIZE * 3) / 2);
+    const text = new PIXI.Text('Spin!', style);
+    text.position = { x: top.width / 2, y: top.height / 2 };
+    top.addChild(text);
+  }, []);
+  const drawBottom = useCallback((bottom: PIXI.Graphics) => {
+    bottom.beginFill(0, 1);
+    bottom.drawRect(
+      0,
+      SYMBOL_SIZE * 3 + (height - SYMBOL_SIZE * 3) / 2,
+      width,
+      (height - SYMBOL_SIZE * 3) / 2
+    );
+    // const text = new PIXI.Text('Spin!', style);
+    // text.position = { x: 10, y: 10 };
+    // bottom.addChild(text);
+  }, []);
+  console.log(Math.round((width - REEL_WIDTH * 5) / 2));
   return (
-    <div>
-      <Stage onMount={setApp}>
-        {app && (
-          <>
-            <Container
-              position={[
-                Math.round(app.screen.width - REEL_WIDTH * 5),
-                (app.screen.height - SYMBOL_SIZE * 3) / 2,
-              ]}
-            >
-              {slotSymbols}
-            </Container>
-            <Graphics draw={drawTop} />
-            <Graphics draw={drawBottom} />
-          </>
-        )}
-      </Stage>
-    </div>
+    <Stage onMount={setApp} width={width} height={height}>
+      {app && (
+        <>
+          <Graphics draw={drawTop} />
+          <Container position={[Math.round(width / 6), (height - SYMBOL_SIZE * 3) / 2]}>
+            {slotSymbols}
+          </Container>
+          <Graphics draw={drawBottom} />
+        </>
+      )}
+    </Stage>
   );
 };
 
--- src/consts.ts
+++ src/consts.ts
@@ -1,15 +1,19 @@
-import { TextStyle } from "pixi.js";
+import { TextStyle, Texture } from 'pixi.js';
+import a from './assets/symbols/a.png';
+import k from './assets/symbols/k.png';
+import q from './assets/symbols/q.png';
 
 export const lineCount = 5;
 export const lineLength = 3;
 export const defaultSlotArr = new Array(lineCount).fill(0).map(() => new Array(lineLength).fill(0));
 
-export const slotTextures = [
-  './assets/symbols/a.png',
-  './assets/symbols/k.png',
-  './assets/symbols/q.png',
-  './assets/symbols/j.png',
-];
+export const width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+export const height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+export const slotTextures = [a, k, q, k];
+
+export const randomTexture = () =>
+  Texture.from(slotTextures[Math.floor(Math.random() * slotTextures.length)]);
 
 export function lerp(a1: number, a2: number, t: number): number {
   return a1 * (1 - t) + a2 * t;
@@ -34,3 +38,61 @@ export const style = new TextStyle({
   wordWrap: true,
   wordWrapWidth: 440,
 });
+
+export const tweening: ITween[] = [];
+
+export interface ITween {
+  object: any;
+  property: string;
+  propertyBeginValue: number;
+  target: number;
+  easing: (t: number) => number;
+  time: number;
+  change?: any;
+  complete?: any;
+  start: number;
+}
+
+export function tweenTo(
+  object: any,
+  property: string,
+  target: number,
+  time: number,
+  easing: (t: number) => number,
+  onchange?: any,
+  oncomplete?: any
+): ITween {
+  const tween: ITween = {
+    object,
+    property,
+    propertyBeginValue: object[property],
+    target,
+    easing,
+    time,
+    change: onchange,
+    complete: oncomplete,
+    start: Date.now(),
+  };
+
+  tweening.push(tween);
+  return tween;
+}
+
+// export const startPlay = (running: boolean, setRunning: React.Dispatch<boolean>) => {
+//   if (running) return;
+//   setRunning(true);
+
+//   reels.forEach((r, i) => {
+//     const extra = Math.floor(Math.random() * 3);
+//     const [target, time] = [r.position + 10 + i * 5 + extra, 2500 + i * 600 + extra * 600];
+//     tweenTo(
+//       r,
+//       'position',
+//       target,
+//       time,
+//       backout(0.5),
+//       null,
+//       i === reels.length - 1 ? setRunning(false) : null
+//     );
+//   });
+// };
